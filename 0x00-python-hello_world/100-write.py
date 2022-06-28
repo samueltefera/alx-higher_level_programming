25de03ae447a333dc7c61b95fe1e6df810493616
Samuel Tefera <samueltefer23@gmail.com>
2022-06-28 09:17:19 +0300

    Update 100-write.py

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 import sys
-sys.stderr.write("and that piece of art is use ful - Dorakorpar, 2015-10-19\n")
+sys.stderr.write("and that piece of art is useful - Dorakorpar, 2015-10-19\n")
 exit(1)
